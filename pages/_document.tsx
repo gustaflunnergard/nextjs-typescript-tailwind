8e2464314de0cba3aea417db2999390af418e95c
Gustaf Lunnergård <gustaf@dbt.se>
2022-01-07 13:51:20 +0100

    Use next.js v 11

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,48 +1,34 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentProps,
-  DocumentContext,
-} from "next/document";
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { extractCritical } from "@emotion/server";
 
-const MyDocument = ({
-  ids,
-  css,
-}: DocumentProps & { ids: string[]; css: string }) => {
-  return (
-    <Html lang="en">
-      <Head>
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const critical = extractCritical(initialProps.html);
+    initialProps.html = critical.html;
+    initialProps.styles = (
+      <React.Fragment>
+        {initialProps.styles}
         <style
-          data-emotion-css={ids?.join(" ")}
-          dangerouslySetInnerHTML={{ __html: css }}
+          data-emotion-css={critical.ids.join(" ")}
+          dangerouslySetInnerHTML={{ __html: critical.css }}
         />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+      </React.Fragment>
+    );
 
-MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const initialProps = await Document.getInitialProps(ctx);
-  const critical = extractCritical(initialProps.html);
-  initialProps.html = critical.html;
-  initialProps.styles = (
-    <>
-      {initialProps.styles}
-      <style
-        data-emotion-css={critical.ids.join(" ")}
-        dangerouslySetInnerHTML={{ __html: critical.css }}
-      />
-    </>
-  );
+    return initialProps;
+  }
 
-  return initialProps;
-};
-
-export default MyDocument;
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
